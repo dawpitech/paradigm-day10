f2d74d2c01d3516d04c47b01a06cfcbf95bffe03
Antoine <me@antoinebellanger.fr>
2025-01-28 10:34:16 +0100

    Initial commit

---
+++ bonus/main.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2025
+** paradigm-day10
+** File description:
+** main.cpp
+*/
+
+#include <iostream>
+
+#include "Almond.hpp"
+#include "Orange.hpp"
+#include "Strawberry.hpp"
+
+int main()
+{
+    Orange o;
+    Strawberry s;
+    const Almond a;
+    IFruit& f = o;
+    std::cout << o.getName() << ": " << o.getVitamins() << " vitamins" << std::endl;
+    std::cout << s << std::endl;
+    std::cout << a << std::endl;
+    o.peel();
+    std::cout << f << std::endl;
+    return 0;
+}
